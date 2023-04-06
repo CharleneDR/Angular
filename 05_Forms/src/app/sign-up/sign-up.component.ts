fa49e85ae0ea3c0091dd9849f8c309ee263be116
CharleneDR <cdarugna@sqli.com>
2023-04-06 20:23:12 +0200

    Quête 05

---
+++ 05_Forms/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent {
+  model: User = new User("", "", "", "");
+
+  constructor() { };
+
+  onSubmit(): void {
+    console.log(this.model);
+  }
+}
